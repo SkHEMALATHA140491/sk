5fdfe8feabbf874af3a54abe2754344f03dbe078
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-06-23 07:42:17 -0700

    Create vowelorconstant.class.java

---
+++ vowelorconstant.class.java
@@ -0,0 +1,10 @@
+package character;
+public class character{
+public static void main(String args[]){
+char chr;
+if(chr='a','e''i','o','u'){
+system.out.println("it is a vowel");
+}
+else{
+system.out.println("it is a consonant");
+}
