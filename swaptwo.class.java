84fe201c504cd98629bcd18736948098884bc7aa
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-04 15:13:41 +0530

    Create swaptwo.class.java

---
+++ swaptwo.class.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.Scanner;
+public class swap{
+public static void main(String args[]){
+int a;
+int b;
+int c;
+scannner s=new Scanner(System.in):
+a=s.nextInt();
+b=s.nextInt();
+system.out.println("enter the value of a&b");
+c=a;
+a=b;
+b=c;
+System.out.println("values of a"+a);
+System.out.println("values of b"+b);
+}
+}
