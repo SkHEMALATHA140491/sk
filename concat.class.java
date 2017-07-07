c3c6e1109d7873abca48dbbc08cfdc4e3debcc14
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-07 15:02:13 +0530

    Create concat.class.java

---
+++ concat.class.java
@@ -0,0 +1,11 @@
+package latha;
+import java.util.Scanner;
+public class latha{
+public static void main(String args[]){
+String s="hema";
+String s1="latha";
+String str=" ";
+str=s+s1;
+System.out.println("result" +str);
+}
+}
