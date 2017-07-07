c82283ab81f803b05afeec51b27614e777285c1b
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-07 14:41:50 +0530

    Create strcmp.class.java

---
+++ strcmp.class.java
@@ -0,0 +1,14 @@
+package guvi;
+public class strcmp{
+public static void main(string args[]){
+String s1="hello";
+String s2="hello";
+if(s1.equals(s2)){
+System.out.println("result"+s1):
+}
+else
+{
+System.out.println("result"+null);
+}
+}
+}
