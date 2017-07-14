296863eca6984270502389f5c9b2a42aa19c876b
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-14 15:21:05 +0530

    Create digitnot.class.java

---
+++ digitnot.class.java
@@ -0,0 +1,16 @@
+package guvi;
+
+public class digitornot {
+public static void main(String args[]){
+	String input1="1atha";
+	char c=input1.charAt(0);
+	String s=String.valueOf(c);
+	if(input1!=s){
+		System.out.println("false");
+	}
+	else
+	{
+		System.out.println("true");
+}
+}
+}
