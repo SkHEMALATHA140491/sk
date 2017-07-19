8d49ff1d141c253b03e318e54056e85d8585e92d
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-19 14:37:32 +0530

    Create count words.class.java

---
+++ count words.class.java
@@ -0,0 +1,17 @@
+package latha;
+
+public class trys {
+	public static void main(String[] args) {
+		String s1="kavi";
+		int count=0;
+		int s=0;
+		
+		for(int i=0;i<s1.length();i++){
+			System.out.println(s1);
+			count++;
+			
+				}
+			
+	System.out.println(count);
+		}
+}
