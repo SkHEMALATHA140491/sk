5f91093305e57204156f1d455b813351f973a610
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-19 14:39:42 +0530

    Update count words.class.java

--- count words.class.java
+++ count words.class.java
@@ -4,10 +4,9 @@ public class trys {
 	public static void main(String[] args) {
 		String s1="kavi";
 		int count=0;
-		int s=0;
 		
 		for(int i=0;i<s1.length();i++){
-			System.out.println(s1);
+			//System.out.println(s1);
 			count++;
 			
 				}
