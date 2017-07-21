d3893b9fbf27c09bdc45aaae6af266abde7faf91
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-21 12:31:04 +0530

    Create countchar.class.java

---
+++ countchar.class.java
@@ -0,0 +1,11 @@
+package guvi;
+public classscountchar{
+public static void main(String args[]){
+int count=0;
+String s="dfgcvjhj345ljh";
+for(int i=0;i<s1.length;i++){
+count++;
+}
+System.out.println(count);
+}
+}
