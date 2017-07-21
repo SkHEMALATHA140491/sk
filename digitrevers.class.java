e1e16e4db21b47d807dba81e94f48b96ee1b86ae
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-21 14:47:23 +0530

    Create digitrevers.class.java

---
+++ digitrevers.class.java
@@ -0,0 +1,22 @@
+package guvi;
+import java.util.Scanner;
+public class revrsedigit{
+public static vpoid main (String args[]){
+int n;
+int m;
+int rem=0;
+int r;
+while(n!-0){
+r=n%10;
+rem=rem*10+r;
+n/=10;
+}
+if(m==r){
+System.out.println("revrse");
+}
+else
+{
+Syustem.out.println("not a reverse");
+}
+}
+}
