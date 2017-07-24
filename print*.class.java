efee901cb571c8ddecaaebd7fb2c3c3631cbd459
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-24 15:31:12 +0530

    Create print*.class.java

---
+++ print*.class.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.Scanner;
+public class  print{
+public static void main(String args[]){
+int n;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(int i=1;i<=n;i++){
+for(int j=1;j<n;j++{
+System.out.println("*");
+}
+System.out.print("*");
+}
+}
+}
