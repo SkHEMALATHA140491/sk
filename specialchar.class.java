d96c4f44bd05969803a4494d7330f67a378e134b
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-24 15:09:48 +0530

    Create specialchar.class.java

---
+++ specialchar.class.java
@@ -0,0 +1,17 @@
+package guvi;
+import java.util.Scanner;
+public class specialchar{
+public static void main(String args[]){
+String ss=" ";
+int count=0;
+Scanner ss=new Scanner(Sysytem.in);
+ss=s.nextLine();
+for(int i=0; i<ss.length();i++){
+char c=ss.charAt(i);
+if(character.isspecialcharacter){
+count++
+}
+}
+System.out.println(count);
+}
+}
