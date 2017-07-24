5b92de1c0a017799c4e339b7bd799ea9318d4fbf
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-24 15:04:15 +0530

    Create char.class.java

---
+++ char.class.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.Scanner;
+public class charactercount{
+public static void main(string args[]){
+String s1=" ";
+Scanner s=new Scanner(System.in);
+s1=s.nextLine();
+for(int i=o; i<s1.length(); i++){
+char c=s1.charAt(i);
+if(character.isdigit){
+count++;
+}
+}
+System.out.println(count);
+}
+}
