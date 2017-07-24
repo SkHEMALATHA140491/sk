f2f8a567ec3c8e1f3a13130b7625bfa1e7151d71
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-24 14:49:03 +0530

    Create charnumal.class.java

---
+++ charnumal.class.java
@@ -0,0 +1,27 @@
+package guvi;
+import java.util.SCanner;
+public class cnal{
+public static void main(String args[]){
+String sss=" ";
+int count=0;
+int p=0;
+int al=0;
+SCanner s=new Scanner (System.in);
+sss=s.nextLine();
+for(int i=0; i<sss.length(); i++){
+char c=sss.charAt(i);
+if(character.isalphabet){
+count++;
+}
+else if(character. isdigit){
+p++;
+}
+else
+{
+al++;
+}
+System.out.println(count);
+System.out.println(p);
+System.out.println(count);
+}
+}
