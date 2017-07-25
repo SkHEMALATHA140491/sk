5ebf6fabc50a3754f6c027354801f2da1553036d
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-25 11:39:04 +0530

    Create hello.class.java

---
+++ hello./hello.class.java
@@ -0,0 +1,16 @@
+pckage guvi;
+public class hello{
+public static void main(String args[]){
+String bs="hello";
+int l=bs.length();
+int v;
+v=l/2;
+StringBuilder result=new StringBuilder();
+char[]ch=s.toCharArray();
+ch[v]='*';
+s=String.valueOf(ch);
+System.out.println(bs);
+String r=new StringBuffer(bs).reverse().toString();
+System.out.println(r);
+}
+}
